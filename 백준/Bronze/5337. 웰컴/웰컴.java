d6f8f6999cc723ea7580b468375bb18743961942
dngyj <123816489+dngyj@users.noreply.github.com>
2023-04-14 17:38:44 +0900

    [Bronze V] Title: 웰컴, Time: 72 ms, Memory: 11204 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	static BufferedReader br;
+
+	static String endl = "\n";
+	static String blank = " ";
+	
+	
+	static void input() throws IOException {
+	}
+
+	static void pro() throws IOException {
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+	
+	public static void main(String[] args) throws IOException {
+		input();
+		pro();
+	}
+
+	static void stk() throws IOException {
+		st = new StringTokenizer(br.readLine());
+	}
+
+}
