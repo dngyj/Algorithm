9897216e4c113dbc35d650af1b36939404dcf344
dngyj <123816489+dngyj@users.noreply.github.com>
2023-05-01 17:26:09 +0900

    [Silver V] Title: 숫자 카드, Time: 844 ms, Memory: 37976 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,48 +1,65 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class Main{
-	static int[] cards;
-	
-	public static void main(String[]args)throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
+	static int N,M;
+	static List<Integer> list = new ArrayList<>();
+	public static void main(String[] args) throws Exception{
 		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		cards = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = read();
 		for(int i = 0; i < N; i++) {
-			cards[i] = Integer.parseInt(st.nextToken());
+			list.add(read());
+		}
+		Collections.sort(list);
+		
+		M = read();
+		for(int i = 0; i < M; i++) {
+			int cur = read();
+			sb.append(binarySearch(cur)).append(' ');
 		}
 		
-		Arrays.sort(cards);
-		
-		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0 ; i < M; i++) {
-			int now = Integer.parseInt(st.nextToken());
-			sb.append(binarySearch(now)).append(' ');
-		}
-		
-		bw.write(sb.toString());
-		bw.close();
+		System.out.println(sb);
 	}
 	
 	static int binarySearch(int target) {
-		int s = 0;
-		int e = cards.length-1;
-		
-		while(s<=e) {
-			int mid = (s+e)/2;
-			
-			if(cards[mid] == target) {
+		int start = 0;
+		int end = N-1;
+		while(start<=end) {
+			int mid = (start+end)/2;
+			if(list.get(mid) == target) {
 				return 1;
 			}
-	
-			if(cards[mid] < target) s = mid + 1;
-			else e = mid - 1;
+			else if(list.get(mid) < target) {
+				start = mid + 1;
+			}
+			else {
+				end = mid - 1;
+			}
 		}
 		
 		return 0;
 	}
+	
+	// 빠른 입력 메서드
+	static int read() throws Exception{
+		int n = 0;
+		boolean isNumber = false;
+		boolean isNegative = false;
+		while(true) {
+			int c = System.in.read();
+			if(c<=32) {
+				if(isNumber) return isNegative?(-1)*n:n;
+				else continue;
+			}
+			else if(c=='-') {
+				isNegative = true;
+			}
+			else {
+				isNumber = true;
+				n = (n<<3) + (n<<1) + (c&15);
+			}
+		}
+	}
+	
 }
