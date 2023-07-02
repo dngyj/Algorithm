58d1c39b17e1e09865112851c5e6ba56b6469f15
DongGyo Jeong <123816489+dngyj@users.noreply.github.com>
2023-07-03 05:14:56 +0900

    [Gold IV] Title: 최단경로, Time: 1944 ms, Memory: 313556 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,90 +1,86 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Main {
-	static final int INF = Integer.MAX_VALUE;
-	static class Node implements Comparable<Node>{ // 노드 클래스 만들기
-		int v, w;
 
-		Node(int v, int w) {
-			this.v = v;
-			this.w = w;
-		}
+    static final int INF = Integer.MAX_VALUE;
+    static int V, E, start;
+    static ArrayList<Node>[] list;
+    static int[] dist;
 
-		@Override
-		public int compareTo(Node o) {
-			return this.w - o.w;
-		}		
-	}
-	
-	static List<ArrayList<Node>> graph = new ArrayList<>();
-	static int[] dist;
-	static PriorityQueue<Node> pq = new PriorityQueue<>();
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		int V = readInt();
-		int E = readInt();
-		for(int i = 0; i < V+1; i++) 
-			graph.add(new ArrayList<>()); // 1~V까지
-		int start = readInt();
-		for(int i = 0; i < E; i++) {
-			int A = readInt();
-			int B = readInt();
-			int C = readInt();
-			graph.get(A).add(new Node(B,C));
-		}
-		
-		dist = new int[V+1];
-		
-		dijkstra(start, V);
-		
-		for(int i = 1; i < V+1; i++) {
-			if(dist[i] == INF) sb.append("INF").append('\n');
-			else sb.append(dist[i]).append('\n');
-		}
-		
-		bw.write(sb.toString());
-		bw.close();
-	}
-	
-	static void dijkstra(int start, int V) {
-		for(int i = 1; i < V+1; i++) {
-			dist[i] = INF; // 초기화
-		}
-		pq.offer(new Node(start,0));
-		dist[start] = 0;
-		int pick = 0;
-		while(!pq.isEmpty()){
-			Node node = pq.poll();
-			
-			if(dist[node.v] < node.w) continue;
-			pick++;
-			if(pick == V) break;
-			
-			for(int i = 0; i < graph.get(node.v).size(); i++) {
-				Node curr = graph.get(node.v).get(i);
-				if(dist[curr.v]>dist[node.v]+curr.w){
-					dist[curr.v] = dist[node.v] + curr.w;
-					pq.offer(new Node(curr.v, dist[curr.v]));
-				}
-			}
-		}
-	}
-    
-    static int readInt() throws IOException {
-        int n = 0;
-        boolean isNegative = false;
-        while (true) {
-            int input = System.in.read();
-            if (input<=32)
-                return isNegative ? n * -1 : n;
-            else if(input=='-')
-                isNegative = true;
-            else
-                n = (n<<3) + (n<<1) + (input&15);
+    public static class Node implements Comparable<Node> {
+
+        int ed;
+        int w;
+
+        public Node(int ed, int w) {
+            this.ed = ed;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return w - o.w;
         }
     }
-}
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        V = sc.nextInt(); // 정점의 개수
+        E = sc.nextInt(); // 간선의 개수
+        start = sc.nextInt(); // 시작 정점의 번호
+
+        list = new ArrayList[V + 1]; // 정점의 인접 리스트
+        for (int i = 1; i <= V; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        dist = new int[V + 1]; // key라고 불린 가장 작은 값 저장하는 용도
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+
+        for (int i = 0; i < E; i++) {
+            int st = sc.nextInt();
+            int ed = sc.nextInt();
+            int w = sc.nextInt();
+            list[st].add(new Node(ed, w));
+        }
+
+        dijkstra();
+
+        for (int i = 1; i <= V; i++) {
+            if (dist[i] == INF) {
+                System.out.println("INF");
+            } else {
+                System.out.println(dist[i]);
+            }
+        }
+    }
+
+
+    private static void dijkstra() {
+            PriorityQueue<Node> queue = new PriorityQueue<>();
+            queue.add(new Node(start, 0));
+            while (!queue.isEmpty()) {
+                Node node = queue.poll();
+                int v = node.ed;
+                int weight = node.w;
+                if (dist[v] < weight) { //지금께 더 가중치가 크면 갱신할 필요가 없다.
+                    continue;
+                }
+                for (int i = 0; i < list[v].size(); i++) {//해당 정점과 연결된 것들 탐색
+                    int vertex2 = list[v].get(i).ed;
+                    int weight2 = list[v].get(i).w + weight;
+                    if (dist[vertex2] > weight2) { //지금께 더 최단경로라면 갱신해준다.
+                        dist[vertex2] = weight2;
+                        queue.add(new Node(vertex2, weight2));
+                    }
+                }
+            }
+        }
+    }
