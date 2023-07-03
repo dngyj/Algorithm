231e556f01bc40c9cb1918925584bca7772d8021
DongGyo Jeong <123816489+dngyj@users.noreply.github.com>
2023-07-03 11:44:15 +0900

    [Gold V] Title: 최소비용 구하기, Time: 1096 ms, Memory: 227792 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+
+    static final int INF = Integer.MAX_VALUE;
+    static int V, E, start;
+    static ArrayList<Node>[] list;
+    static int[] dist;
+
+    public static class Node implements Comparable<Node> {
+
+        int ed;
+        int w;
+
+        public Node(int ed, int w) {
+            this.ed = ed;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return w - o.w;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        V = sc.nextInt(); // 정점의 개수
+        E = sc.nextInt(); // 간선의 개수
+
+        list = new ArrayList[V + 1]; // 정점의 인접 리스트
+        for (int i = 1; i <= V; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        dist = new int[V + 1]; // key라고 불린 가장 작은 값 저장하는 용도
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+
+        for (int i = 0; i < E; i++) {
+            int st = sc.nextInt();
+            int ed = sc.nextInt();
+            int w = sc.nextInt();
+            list[st].add(new Node(ed, w));
+        }
+
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+
+        System.out.println(dijkstra(start, end));
+        
+    }
+
+
+    private static int dijkstra(int start, int end) {
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(start, 0));
+
+        dist[start] = 0;
+        queue.add(new Node(start,0));
+
+        while (!queue.isEmpty()) {
+
+            Node node = queue.poll();
+            int v = node.ed;
+            int weight = node.w;
+            if (dist[v] < weight) { //지금께 더 가중치가 크면 갱신할 필요가 없다.
+                continue;
+            }
+            for (int i = 0; i < list[v].size(); i++) {//해당 정점과 연결된 것들 탐색
+                int vertex2 = list[v].get(i).ed;
+                int weight2 = list[v].get(i).w + weight;
+                if (dist[vertex2] > weight2) { //지금것이 더 최단경로라면 갱신해준다.
+                    dist[vertex2] = weight2;
+                    queue.add(new Node(vertex2, weight2));
+                }
+            }
+        }
+        return dist[end]; // 해당목적지의 dist값을 출력해준다.
+    }
+}
