913dddb0afd287a8fbad92648be33dab12ae9d2e
DongGyo Jeong <123816489+dngyj@users.noreply.github.com>
2023-09-01 21:30:14 +0900

    [Silver III] Title: 문자열 집합, Time: 884 ms, Memory: 68876 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        HashSet<String> set = new HashSet<>();
+
+        for(int i = 0; i<N;i++){
+            set.add(sc.next());
+        }
+
+        int cnt = 0;
+        for(int i =0; i<M;i++){
+            if(set.contains(sc.next())) cnt++;
+        }
+
+        System.out.println(cnt);
+
+    }
+
+}
