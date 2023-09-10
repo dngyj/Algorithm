6f172c6a042ab0c80333ff91b92dbbdb14450c28
DongGyo Jeong <123816489+dngyj@users.noreply.github.com>
2023-09-10 21:32:01 +0900

    [Silver II] Title: 스타트와 링크, Time: 312 ms, Memory: 14480 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,77 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
-	static int min = Integer.MAX_VALUE;
-	static int[][] arr;
-	static boolean[] visit;
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N][N];
-		visit = new boolean[N];
-		
-		for(int i = 0 ; i < N ; i++) {
-		StringTokenizer str = new StringTokenizer(br.readLine());
-		for(int j = 0 ; str.hasMoreTokens();j++) {
-			arr[i][j]= Integer.parseInt(str.nextToken());
-		}
-		}
-		
-		dfs(0,0);
-		
-		System.out.println(min);
-		
+    static boolean[] visited;
 
-}
-	public static void dfs(int depth, int a) {
-		
-		if(depth == N/2) {
-			diff();
-			return;
-		}
-		
-		for(int i = a ; i < N ; i++) {
-			visit[i]=true;
-			dfs(depth+1, i+1);
-			visit[i]=false;
-		}	
-	}
-	
-	public static void diff() {
-		int start = 0;
-		int link = 0;
-		for(int i = 0 ; i < N-1 ; i++) {
-			for(int j = i+1 ; j < N ; j++) {
-				if(visit[i]==true && visit[j]==true) {
-					start += arr[i][j];
-					start += arr[j][i];
-				}
-				else if(visit[i]==false && visit[j]==false) {
-					link += arr[i][j];
-					link += arr[j][i];
-				}
-				
-			}
-		}
-		
-		int val = Math.abs(start - link);
-		
-		if(val == 0) {
-			System.out.println(val);
-			System.exit(0);
-		}
-		
-		min=Math.min(min,val);
-	}
-	
+    static int[][] abilities;
+    static int N;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        visited = new boolean[N];
+        abilities = new int[N][N];
+
+        for(int i=0;i<N;i++){
+            StringTokenizer str = new StringTokenizer(br.readLine());
+            for(int j=0;str.hasMoreTokens();j++){
+                abilities[i][j] = Integer.parseInt(str.nextToken());
+            }
+        }
+        comb(0,0);
+
+        System.out.println(min);
+
+    }
+
+    static void comb(int depth, int st){
+
+        if(depth==N/2){
+            diff();
+            return;
+        }
+
+        for(int i=st;i<N;i++){
+            visited[i] = true;
+            comb(depth+1,i+1);
+            visited[i] = false;
+        }
+    }
+
+    private static void diff() {
+
+        int Tstart = 0;
+        int Tlink = 0;
+
+        for(int i=0;i<N-1;i++){
+            for(int j=i+1;j<N;j++){
+                if(visited[i]==true&&visited[j]==true){
+                    Tstart += abilities[i][j];
+                    Tstart += abilities[j][i];
+                }
+                else if(visited[i]==false&&visited[j]==false){
+                    Tlink += abilities[i][j];
+                    Tlink += abilities[j][i];
+                }
+            }
+        }
+
+        int value = Math.abs(Tstart-Tlink);
+
+        min = Math.min(min,value);
+
+    }
 
 }
