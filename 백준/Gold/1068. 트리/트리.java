94490b089c9e2c1ecf1bb04810c5c79485389e9c
DongGyo Jeong <123816489+dngyj@users.noreply.github.com>
2023-11-24 13:36:13 +0900

    [Gold V] Title: 트리, Time: 116 ms, Memory: 12904 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,53 +1,52 @@
-import java.util.*;
- 
+import java.util.Scanner;
+
 public class Main {
- 
-    static int n, delete;
-    static int[] parent;
-    static int count;
+    static int[] parents;
     static boolean[] visited;
-    
+    static int remove, cnt, N;
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        n = scan.nextInt();
-        parent = new int[n];
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
         int root = 0;
-        for(int i = 0; i < n; i++) {
-            parent[i] = scan.nextInt();        
-            if(parent[i] == -1) root = i;
+        parents = new int[N];
+
+        for(int i = 0; i<N; i++){
+            parents[i] = sc.nextInt();
+            if(parents[i]==-1) root = i;
         }
-        delete = scan.nextInt();
-        
-        deleteNode(delete);
-            
-        count = 0;
-        visited = new boolean[n];
-        countLeaf(root);
-            
-        System.out.println(count);
+        remove = sc.nextInt();
+
+        deleteNode(remove);
+        cnt = 0;
+        visited = new boolean[N];
+        findLeaf(root);
+
+        System.out.println(cnt);
     }
-    
-    public static void deleteNode(int d) {
-        parent[d] = -2; //삭제된 노드 -2로 표시
-        for(int i = 0; i < n; i++) {
-            if(parent[i] == d) {
+
+    public static void deleteNode(int remove){
+        parents[remove] = -2;
+        for(int i = 0; i<N; i++){
+            if(parents[i]==remove){
                 deleteNode(i);
             }
         }
     }
-    
-    public static void countLeaf(int s) {
-        boolean isLeaf = true;
-        visited[s] = true;
-        if(parent[s] != -2) {
-            for(int i = 0; i < n; i++) {
-                if(parent[i] == s && visited[i] == false) {
-                    countLeaf(i);
-                    isLeaf = false;
+
+    public static void findLeaf(int root){
+        visited[root] = true;
+        boolean leaf = true;
+        if(parents[root]!=-2){
+            for(int i = 0; i<N; i++){
+                if(parents[i]==root&&!visited[i]){
+                    findLeaf(i);
+                    leaf = false;
                 }
             }
-            if(isLeaf) count++;
+            if(leaf) cnt++;
         }
+
     }
 }
