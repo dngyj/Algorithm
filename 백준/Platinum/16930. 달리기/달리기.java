dcf69a57fe9b3c68b9a4bdeed8feded872fb9b01
DongGyo Jeong <123816489+dngyj@users.noreply.github.com>
2023-11-26 15:24:05 +0900

    [Platinum III] Title: 달리기, Time: 804 ms, Memory: 106460 KB -BaekjoonHub

--- 백준/Platinum/16930. 달리기/달리기.java
+++ 백준/Platinum/16930. 달리기/달리기.java
@@ -1,72 +1,81 @@
-import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
-	static int[][] arr;
-	static int[] dr = {-1, 0, 1, 0};
-	static int[] dc = {0, 1, 0, -1};
-	static int N,M,K;
-	static int min = -1;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		arr = new int[N][M];
-		for(int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for(int j = 0; j < M; j++) {
-				if(str.charAt(j) == '#') arr[i][j] = -1;
-			}
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		
-		bfs(Integer.parseInt(st.nextToken())-1,Integer.parseInt(st.nextToken())-1,Integer.parseInt(st.nextToken())-1,Integer.parseInt(st.nextToken())-1);
-		
-		System.out.println(min);
-	}
-	
-	static void bfs(int sR, int sC, int eR, int eC) {
-		Queue<int[]> queue = new LinkedList<>();
-		arr[sR][sC] = 0;
-		queue.offer(new int[] {sR,sC});
-		
-		while(!queue.isEmpty()) {
-			int[] now = queue.poll();
-			
-			int r = 0;
-			int c = 0;
-			for(int i = 0; i < 4; i++) {
-				for(int j = 1; j <= K; j++) {
-					r = now[0] + j*dr[i];
-					c = now[1] + j*dc[i];
-					if(!check(r,c) || arr[r][c] == -1) break;
-					
-					if(arr[r][c] == 0) {
-						arr[r][c] = arr[now[0]][now[1]] + 1;
-						if(r==eR && c==eC) {
-							min = arr[r][c];
-							return;
-						}
-						else queue.offer(new int[] {r,c});
-					}
-					else if(arr[r][c] < arr[now[0]][now[1]]+1) break;
-				}
-			}
-	
-		}
-	}
-	
-	static boolean check(int r, int c) {
-		if(r<0 || r>=N || c<0 || c>=M) return false;
-		else return true;
-	}
-	
-	static void see() {
-		for(int i = 0; i< N; i++) {
-			System.out.println(Arrays.toString(arr[i]));
-		}
-	}
+
+    static int N,M,K;
+    static char[][] map;
+    static int[][] visited;
+    static int r1,r2,c1,c2;
+    static int cnt;
+    static boolean[] cantgo;
+
+    static Queue<int[]> queue = new LinkedList<>();
+
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0, 0, 1, -1};
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        K = sc.nextInt();
+
+        map = new char[N][M];
+        visited = new int[N][M];
+
+        for(int r = 0; r<N;r++){
+            String str = sc.next();
+            Arrays.fill(visited[r], Integer.MAX_VALUE);
+            for(int c = 0; c<M; c++){
+                map[r][c] = str.charAt(c);
+            }
+        }
+        r1 = sc.nextInt()-1;
+        c1 = sc.nextInt()-1;
+        r2 = sc.nextInt()-1;
+        c2 = sc.nextInt()-1;
+
+        queue.add(new int[]{r1,c1, 0});
+        visited[r1][c1] = 0;
+        bfs();
+
+    }
+    public static void bfs(){
+
+        while(!queue.isEmpty()){
+            cantgo = new boolean[4];
+            int[] info = queue.poll();
+            int r = info[0];
+            int c = info[1];
+            int cnt = info[2];
+
+            if(r==r2&&c==c2){
+                System.out.println(cnt);
+                return;
+            }
+            for(int d=0;d<4;d++){
+                 for(int k=1; k<=K;k++){
+                    if(cantgo[d]) continue;
+                    int idr = r+dr[d]*k;
+                    int idc = c+dc[d]*k;
+
+                    if(idr<0||idc<0||idr>=N||idc>=M) continue;
+                    if(map[idr][idc]=='#') {
+                        cantgo[d] = true;
+                        continue;
+                    }
+                    if(visited[idr][idc]<visited[r][c]+1) break;
+                    if(map[idr][idc]=='.'&&visited[idr][idc]==Integer.MAX_VALUE){
+                        visited[idr][idc] = visited[r][c]+1;
+                        queue.add(new int[]{idr,idc, cnt+1});
+                    }
+                }
+            }
+        }
+        System.out.println(-1);
+    }
 }
