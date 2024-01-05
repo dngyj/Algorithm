fc89ad8289b7bcaf3d6711a525c4be2ac0f88a2e
DongGyo Jeong <123816489+dngyj@users.noreply.github.com>
2024-01-05 22:18:06 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 152 ms, Memory: 17916 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -1,135 +1,80 @@
-import java.util.*;
-import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-	static BufferedReader br;
 
-	static String endl = "\n";
-	static String blank = " ";
-	static int N, M;
-	static int ans;
-	static boolean flag;
-	static int[] key;
-	static char[][] map;
-	static boolean[][][][][][][][] visited;
-	static int[] dr = { -1, 0, 1, 0 };
-	static int[] dc = { 0, 1, 0, -1 };
+    static int N,M;
+    static char[][] map;
+    static boolean[][][] visited;
+    static boolean[] key = new boolean[6];
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,1,-1};
+    static Queue<int[]> queue = new LinkedList<>();
 
-	// . : 빈칸, # : 벽, a~f : 열쇠, A~F : 문, 0 : 민식, 1 : 출구
-	static void input() throws IOException {
-		br = new BufferedReader(new InputStreamReader(System.in));
-		stk();
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new char[N][M];
-		key = new int[6];
-		visited = new boolean[2][2][2][2][2][2][N][M];
-		for (int r = 0; r < N; r++) {
-			String str = br.readLine();
-			for (int c = 0; c < M; c++) {
-				map[r][c] = str.charAt(c);
-			}
-		}
-	}
+    public static void main(String[] args) {
 
-	static void pro() throws IOException {
-		ans = Integer.MAX_VALUE;
-		Outer: for (int r = 0; r < N; r++) {
-			for (int c = 0; c < M; c++) {
-				if (map[r][c] == '0') {
-					bfs(r, c);
-					if (flag) {
-						System.out.println(ans);
-					} else {
-						System.out.println(-1);
-					}
-					break Outer;
-				}
-			}
-		}
-	}
+        Scanner sc = new Scanner(System.in);
 
-	static void bfs(int x, int y) {
-		Queue<Point> q = new ArrayDeque<>();
-		q.offer(new Point(x, y, 0, new int[6]));
-		visited[0][0][0][0][0][0][x][y] = true;
-		int temp = 0;
-		while (!q.isEmpty()) {
-			Point p = q.poll();
-			// 도착 지점
-			if (map[p.r][p.c] == '1') {
-				flag = true;
-				ans = Math.min(ans, p.move);
-				break;
-			}
-			int[] nKey = p.key;
-			int a = p.key[0];
-			int b = p.key[1];
-			int c = p.key[2];
-			int d = p.key[3];
-			int e = p.key[4];
-			int f = p.key[5];
-			int move = p.move;
-			int nr, nc;
-			for (int i = 0; i < 4; i++) {
-				nr = p.r + dr[i];
-				nc = p.c + dc[i];
-				if (!inRange(nr, nc) || map[nr][nc] == '#') continue;
-				char ch = map[nr][nc];
-				if ('A' <= ch && ch <= 'F') {
-					if (visited[a][b][c][d][e][f][nr][nc]) continue;
-					// 열쇠 있어? 있으면 열고 가
-					if (nKey[ch - 'A'] != 0) {
-						q.offer(new Point(nr, nc, move + 1, nKey));
-						visited[a][b][c][d][e][f][nr][nc] = true;
-					}
-					// 없으면 못가
-				}
-				// 탐색하는데 열쇠가 있어 => 줍줍
-				else if ('a' <= ch && ch <= 'f') {
-					if (visited[a][b][c][d][e][f][nr][nc]) continue;
-					int[] key = new int[6];
-					for (int j = 0; j < key.length; j++) {
-						key[j] = nKey[j];
-					}
-					key[ch - 'a'] = 1;
-					q.offer(new Point(nr, nc, move + 1, key));
-					visited[key[0]][key[1]][key[2]][key[3]][key[4]][key[5]][nr][nc] = true;
-				} else {
-					if (visited[a][b][c][d][e][f][nr][nc]) continue;
-					q.offer(new Point(nr, nc, move + 1, nKey));
-					visited[a][b][c][d][e][f][nr][nc] = true;
-				}
-			}
-		}
-	}
+        N = sc.nextInt();
+        M = sc.nextInt();
 
-	static boolean inRange(int r, int c) {
-		return 0 <= r && r < N && 0 <= c && c < M;
-	}
+        map = new char[N][M];
+        visited = new boolean[N][M][64];
 
-	static class Point {
-		int r, c, move;
-		int[] key;
+        for(int r = 0; r<N;r++){
+            String str = sc.next();
+            for(int c=0; c<M;c++){
+                map[r][c] = str.charAt(c);
+                if(map[r][c]=='0') {
+                    queue.add(new int[]{r,c,0,0});
+                    map[r][c] ='.';
+                }
+            }
+        }
+        System.out.println(bfs());
 
-		public Point(int r, int c, int move, int[] key) {
-			super();
-			this.r = r;
-			this.c = c;
-			this.move = move;
-			this.key = key;
-		}
-	}
+    }
 
-	public static void main(String[] args) throws IOException {
-		input();
-		pro();
-	}
+    public static int bfs(){
 
-	static void stk() throws IOException {
-		st = new StringTokenizer(br.readLine());
-	}
+        while(!queue.isEmpty()){
+            int[] info = queue.poll();
+            int r = info[0];
+            int c = info[1];
+            int depth = info[2];
+            int key = info[3];
 
+            visited[r][c][key] = true;
+
+            if(map[r][c]=='1') return depth;
+
+            for(int d=0;d<4;d++){
+                int idr = r+dr[d];
+                int idc = c+dc[d];
+
+                if(idr<0||idc<0||idr>=N||idc>=M) continue;
+                if(map[idr][idc]=='#') continue;
+                if(visited[idr][idc][key]) continue;
+
+                if(map[idr][idc]>='a'&&map[idr][idc]<='f'){
+                    int newKey = key | (1<<(map[idr][idc]-'a'));
+                    queue.add(new int[]{idr,idc,depth+1, newKey});
+                    visited[idr][idc][newKey] = true;
+                }
+
+                if(map[idr][idc]>='A'&&map[idr][idc]<='F') {
+                    if((key & (1<<(map[idr][idc]-'A')))!=0){
+                        queue.add(new int[]{idr,idc,depth+1, key});
+                        visited[idr][idc][key] = true;
+                    }
+                }
+                else{
+                    visited[idr][idc][key] = true;
+                    queue.add(new int[]{idr,idc,depth+1,key});
+                }
+            }
+        }
+        return -1;
+    }
 }
