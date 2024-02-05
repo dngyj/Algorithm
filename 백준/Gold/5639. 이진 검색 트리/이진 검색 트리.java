a84c9539e96df19ecc4a13234ba89d8150ea51d5
DongGyo Jeong <123816489+dngyj@users.noreply.github.com>
2024-02-05 22:21:53 +0900

    [Gold V] Title: 이진 검색 트리, Time: 844 ms, Memory: 21192 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static class Node{
+        int num;
+        Node left, right;
+
+        Node(int num){
+            this.num=num;
+        }
+        Node(int num,Node left,Node right){
+            this.num=num;
+            this.left=left;
+            this.right=right;
+        }
+        void insert(int n){
+            if(n<this.num){
+                if(this.left==null)
+                    this.left=new Node(n);
+                else this.left.insert(n);
+            }else{
+                if(this.right==null)
+                    this.right=new Node(n);
+                else this.right.insert(n);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        Node root=new Node(Integer.parseInt(br.readLine()));
+        String s;
+        while (true){
+            s=br.readLine();
+            if(s==null||s.equals("")) break;
+            root.insert(Integer.parseInt(s));
+        }
+        postOrder(root);
+    }
+    static void postOrder(Node node){
+        if(node==null) return;
+        postOrder(node.left); 
+        postOrder(node.right);
+        System.out.println(node.num);
+    }
+}
