a6427a171fdd1846cea3783bb8a86b97ee933eb4
DongGyo Jeong <123816489+dngyj@users.noreply.github.com>
2024-02-12 15:01:19 +0900

    [Gold V] Title: 용액, Time: 832 ms, Memory: 99124 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] waters = new int[N];
+        for(int i =0;i<N;i++){
+            waters[i] = sc.nextInt();
+        }
+        Arrays.sort(waters);
+
+        int left = 0;
+        int right = N-1;
+        int ans1 = 0;
+        int ans2 = 0;
+        int min = Integer.MAX_VALUE;
+        int dist;
+        while(left<right){
+            dist = waters[right]+waters[left];
+            int Dist = Math.abs(dist);
+            if(Dist < min){
+                min = Dist;
+                ans1 = waters[left];
+                ans2 = waters[right];
+            }
+            if(dist>0) right--;
+            else left++;
+        }
+        System.out.println(ans1 +" "+ ans2);
+    }
+}
