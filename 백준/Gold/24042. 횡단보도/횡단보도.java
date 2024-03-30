a38df2e9c12a97b64b6d514c937fefde57607cb2
정동교 <123816489+dngyj@users.noreply.github.com>
2024-03-30 13:23:12 +0900

    [Gold I] Title: 횡단보도, Time: 2556 ms, Memory: 364416 KB -BaekjoonHub

--- 백준/Gold/24042. 횡단보도/횡단보도.java
+++ 백준/Gold/24042. 횡단보도/횡단보도.java
@@ -9,14 +9,14 @@ public class Main {
     static Long INF = Long.MAX_VALUE;
     static class Node implements Comparable<Node>{
         int v;
-        long w;
-        Node(int v, long w){
+        long time;
+        Node(int v, long time){
             this.v=v;
-            this.w=w;
+            this.time=time;
         }
         @Override
         public int compareTo(Node o){
-            return Long.compare(w,o.w);
+            return (int)(time-o.time);
         }
     }
     static ArrayList<Node>[] list;
@@ -32,7 +32,7 @@ public class Main {
         for(int i =0;i<N+1;i++){
             list[i] = new ArrayList<>();
         }
-        for(int i=0;i<M;i++){
+        for(int i=1;i<=M;i++){
             int st = sc.nextInt();
             int ed = sc.nextInt();
             list[st].add(new Node(ed,i));
@@ -48,16 +48,18 @@ public class Main {
 
         while(!queue.isEmpty()){
             Node current = queue.poll();
-            if(dist[current.v]<current.w) continue;
+            if(dist[current.v]<current.time) continue;
 
             for(Node next:list[current.v]){
+                long order = current.time%M; //현재 시간이 주기내에서 몇번째 위치인지
+                order = next.time-order; // 다음에 올 순서와 지금의 순서를 뺴준다.
+                if(order<0) order+=M; //다음시간이 temp보다 작으면, 즉 순서가 낮으면 주기 더해주기 음수 값이 나온다는 것은 이미 다음 이동 순서가 현재 주기에서 지나갔다는 것
+                long nextTime = order+current.time; // 최종적으로 다음 노드에 도달하기까지의 총 시간을 계산
+                //이는 다음 노드로 이동할 때까지 기다려야 하는 총 시간
 
-                if(next.w>=current.w) next.w = next.w+1;
-                else next.w = ((long)Math.ceil(((double)current.w-next.w)/M))*M+next.w+1;
-
-                if(dist[next.v]>next.w){
-                    dist[next.v] = next.w;
-                    queue.add(new Node(next.v,next.w));
+                if(dist[next.v]>nextTime){
+                    dist[next.v] = nextTime;
+                    queue.add(new Node(next.v,nextTime));
                 }
             }
         }
