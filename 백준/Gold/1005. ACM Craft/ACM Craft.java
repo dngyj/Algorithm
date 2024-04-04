e643d196f0ffc2ca920182fa19d98f18e2173a89
정동교 <123816489+dngyj@users.noreply.github.com>
2024-04-04 10:31:19 +0900

    [Gold III] Title: ACM Craft, Time: 1888 ms, Memory: 301132 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -1,79 +1,64 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
-	static int N, K;
-	static int[] time;
-	static int[] prefix;
-	static List<ArrayList<Integer>> adjList = new ArrayList<>();
-	public static void main(String[] args) throws Exception{
-		int T = read();
-		for(int i = 0; i < T; i++) {
-			N = read(); K = read();
-			for(int j = 0; j <= N; j++) {
-				adjList.add(new ArrayList<>());
-				// N개의 건물에 대해서 inDegree 인접 리스트를 선언
-			}
-			time = new int[N+1];
-			// 각 건물의 단독 건설 시간
-			prefix = new int[N+1];
-			// 각 건물의 이어지는 건설 시간
-			for(int j = 1; j <= N; j++) {
-				time[j] = read();
-			}
+    static Scanner sc = new Scanner(System.in);
+    static int T,N,K,W;
+    static int[] indegree;
+    static int[] time;
+    static int[] dp;
+    static ArrayList<Integer>[] list;
 
-			for(int j = 0; j < K; j++) {
-				int V = read();
-				int E = read();
-				adjList.get(E).add(V);
-				// inDegree
-			}
-			
-			tp();
-			
-			System.out.println(prefix[read()]);
-		}
-	}
-	
-	public static void tp() {
-		Queue<Integer> queue = new LinkedList<>();
-		for(int i = 1; i <=N; i++) {
-			if(adjList.get(i).size() == 0) {
-				prefix[i] = time[i];
-				queue.offer(i);
-				// size()가 0이면 queue에 넣고, inDegree가 없으면 time=prefix.
-			}
-		}
-		
-		while(!queue.isEmpty()) {
-			int cur = queue.poll();
-			for(int i = 1; i <=N; i++) {
-				if(adjList.get(i).contains(cur)) {
-					adjList.get(i).remove((Integer)cur);
-					prefix[i] = Math.max(prefix[i], prefix[cur]+time[i]);
-					if(adjList.get(i).size() == 0) queue.offer(i);
-				}
-			}
-		}
-	}
-	
-	// 비트 연산자를 이용하여 빨리 입력받는 메서드입니다.
-	public static int read() throws Exception{
-		int n = 0;
-		boolean isNumber = false;
-		boolean isNegative = false;
-		while(true) {
-			int c = System.in.read();
-			if(c<=32) {
-				if(!isNumber) continue;
-				return isNegative?(-1)*n:n;
-			}
-			else if(c=='-') {
-				isNegative = true;
-			}
-			else {
-				if(!isNumber) isNumber=true;
-				n = (n<<3) + (n<<1) + (c&15);
-			}
-		}
-	}
+    public static void main(String[] args) {
+        T = sc.nextInt();
+        for(int tc=0;tc<T;tc++){
+            input();
+            topological();
+            System.out.println(dp[W]);
+        }
+    }
+
+    static void input(){
+        N = sc.nextInt();
+        K = sc.nextInt();
+        indegree = new int[N+1];
+        time = new int[N+1];
+        dp = new int[N+1];
+        for(int i =1;i<N+1;i++){
+            time[i] = sc.nextInt();
+        }
+        list = new ArrayList[N+1];
+        for(int i =1;i<N+1;i++){
+            list[i] = new ArrayList<>();
+        }
+        for(int i =0;i<K;i++){
+            int st = sc.nextInt();
+            int ed = sc.nextInt();
+            list[st].add(ed);
+            indegree[ed]++;
+        }
+        W = sc.nextInt();
+    }
+
+    static void topological(){
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 1;i<N+1;i++){
+            if(indegree[i]==0) {
+                dp[i] = time[i];
+                queue.add(i);
+            }
+        }
+        while(!queue.isEmpty()){
+            int cur = queue.poll();
+            for(int next:list[cur]){
+                indegree[next]--;
+                dp[next] = Math.max(dp[cur]+time[next],dp[next]);
+                if(indegree[next]==0){
+                    queue.add(next);
+                }
+            }
+        }
+    }
 }
