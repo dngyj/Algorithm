bf841f823d40e1bfbec611a482fdf19715f8bbdd
정동교 <123816489+dngyj@users.noreply.github.com>
2024-04-15 07:40:58 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 1772 ms, Memory: 316872 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] nums = new int[N];
+        int[] LIS = new int[N];
+        for(int i = 0;i<N;i++){
+            nums[i] = sc.nextInt();
+        }
+        LIS[0] = nums[0];
+        int length = 0;
+
+        for(int i = 1;i<N;i++){
+            if(nums[i]>LIS[length]){
+                length++;
+                LIS[length] = nums[i];
+            }
+            else{
+                int left = 0;
+                int right = length;
+                while(left<right){
+                    int mid = (left+right)/2;
+                    if (nums[i] > LIS[mid]) {
+                        left = mid+1;
+                    }
+                    else{
+                        right = mid;
+                    }
+                }
+                LIS[left] = nums[i];
+            }
+        }
+        System.out.println(length+1);
+    }
+}
