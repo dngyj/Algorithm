2e2ed68d5c5c71810fef3eecff270e49f3c5b2a5
정동교 <123816489+dngyj@users.noreply.github.com>
2024-04-15 07:21:10 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 1560 ms, Memory: 317256 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,66 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
-	
-	static int N;
-	static int[] dp;
-	
-	public static void main(String[] args) throws Exception {
-		N = read(); dp = new int[N+1];
-		int max = 0;
-		dp[1] = read();
-		if(N == 1) {
-			System.out.println(1);
-			return;
-		}
-		for(int i = 2; i <= N; i ++) {
-			dp[i] = Integer.MAX_VALUE;
-			int now = read();
-			max = Math.max(binarySearch(now, i), max);
-		}
-		
-		System.out.println(max);
-		
-	}
-	
-	static int binarySearch(int now, int bound) {
-		int ans = 0;
-		int s = 1;
-		int e = bound;
-		while(s<=e) {
-			int mid = (s+e)/2;
-			if(dp[mid] < now) {
-				ans = mid;
-				s = mid+1;
-			}
-			else {
-				e = mid-1;
-			}
-		}
-		dp[ans+1] = now;
-		return ans+1;
-	}
-	
-	// 빠른 입력 메서드
-	static int read() throws Exception{
-		int n = 0;
-		boolean isNegative = false;
-		boolean isNumber = false;
-		
-		while(true) {
-			int c = System.in.read();
-			if(c <= 32) {
-				if(isNumber) return isNegative?(-1)*n:n;
-				else continue;
-			}
-			
-			else if(c == '-') {
-				isNegative = true;
-			}
-			
-			else {
-				if(!isNumber) isNumber = true;
-				n = (n<<3) + (n<<1) + (c&15);
-			}
-		}
-	}
-	
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] nums = new int[N];
+        int[] LIS = new int[N];
+        for(int i = 0;i<N;i++){
+            nums[i] = sc.nextInt();
+        }
+        LIS[0] = nums[0];
+        int length = 0;
+
+        for(int i = 1;i<N;i++){
+
+            if(nums[i]>LIS[length]){
+                length++;
+                LIS[length] = nums[i];
+            }
+            else{
+                int left = 0;
+                int right = length;
+                while(left<right){
+                    int mid = (left+right)/2;
+                    if (nums[i] > LIS[mid]) {
+                        left = mid+1;
+                    }
+                    else{
+                        right = mid;
+                    }
+                }
+                LIS[left] = nums[i];
+            }
+        }
+        System.out.println(length+1);
+    }
+
+
 }
