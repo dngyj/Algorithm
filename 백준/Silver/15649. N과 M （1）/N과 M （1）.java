809a450b9f7dbbb1a67515dcbc6fa8a8d0c21d5a
정동교 <123816489+dngyj@users.noreply.github.com>
2024-04-21 11:59:26 +0900

    [Silver III] Title: N과 M (1), Time: 224 ms, Memory: 24160 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,43 +1,45 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-	static int N, M;
-	static int[] selected;
-	static boolean[] flag;
-	
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		selected = new int[M+1];
-		flag = new boolean[N+1];
-		
-		function(1);
-		System.out.println(sb.toString());
-	}
-	
-	static void function(int a) {
-		if(a ==M+1) { // a가 원소 끝에 꽉차면
-			for(int i =1;i<=M;i++) sb.append(selected[i]).append(" ");
-			sb.append("\n");
-			//selected 원소 1부터 M까지 출력하기
-		}
-		else { // 아니라면 
-		//	1 부터 N까지 돌면서
-			for(int ele = 1; ele<=N; ele++) {
-				if(!flag[ele]) {
-					selected[a] = ele;
-					flag[ele] = true;
-					function(a+1);
-					flag[ele] =false;
-				}
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
 
-			}
-		}
-	}
-	
+    static boolean[] visited;
+    static int[] selected;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        visited = new boolean[N];
+        selected = new int[M];
+
+        powerset(0);
+        System.out.println(sb);
+    }
+
+    public static void powerset(int index){
+
+        if(index == M){
+            for(int i = 0 ; i<M; i++){
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        else{
+            for(int i = 0; i<N;i++){
+                if(!visited[i]){
+                    selected[index] = i+1;
+                    visited[i] = true;
+                    powerset(index+1);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+
 }
