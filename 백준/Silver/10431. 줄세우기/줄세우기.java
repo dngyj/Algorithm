5b3bee32957f1ea4073d972d68da0904379c8ad4
정동교 <123816489+dngyj@users.noreply.github.com>
2024-05-20 11:17:40 +0900

    [Silver V] Title: 줄세우기, Time: 432 ms, Memory: 34404 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -1,35 +1,19 @@
-import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         for(int t=0;t<T;t++){
             int tc = sc.nextInt();
-            int max = -1;
-            int[] arr = new int[21];
+            int[] arr = new int[20];
+            for(int i = 0;i<20;i++) {
+                arr[i] = sc.nextInt();
+            }
             int count = 0;
-            for(int i = 1;i<=20;i++){
-                int num = sc.nextInt();
-                if(num>max) {
-                    arr[i] = num;
-                    max = num;
-                }
-                else{
-                    int insertIdx = -1;
-                    for(int j=i-1;j>=0;j--){
-                        if(arr[j]<num) {
-                            insertIdx = j+1;
-                            break;
-                        }
-                    }
-                    for(int j=i-1; j>=insertIdx; j--){
-                        arr[j+1] = arr[j];
-                        count++;
-                    }
-                    arr[insertIdx] = num;
+            for(int j=0;j<20;j++){
+                for(int k=0;k<j;k++){
+                    if(arr[j]<arr[k]) count++;
                 }
             }
             System.out.println(tc+" "+count);
