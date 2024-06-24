10eddb89548fb5f49d6e756cd85af873b1447454
정동교 <123816489+dngyj@users.noreply.github.com>
2024-06-24 21:06:48 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,7 +1,3 @@
 function solution(my_string, overwrite_string, s) {
-    let answer = '';
-    answer += my_string.substring(0, s);
-    answer += overwrite_string;
-    answer += my_string.substring(answer.length, my_string.length);
-    return answer;
+    return my_string.slice(0,s)+overwrite_string+my_string.slice(s+overwrite_string.length);
 }
