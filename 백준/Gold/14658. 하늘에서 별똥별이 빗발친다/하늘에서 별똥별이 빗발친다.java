3be17df17301538f453aabc438d6f46c544bc834
정동교 <123816489+dngyj@users.noreply.github.com>
2024-07-09 13:02:47 +0900

    [Gold III] Title: 하늘에서 별똥별이 빗발친다, Time: 168 ms, Memory: 13644 KB -BaekjoonHub

--- 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.java
+++ 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.java
@@ -6,9 +6,32 @@ public class Main {
 
     static int N ,M, L, K;
     static List<int[]> stars = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        input();
+        int max = -1;
+        for(int i = 0; i<K;i++){
+            for(int j = 0; j<K;j++){
+                int r = stars.get(i)[0];
+                int c = stars.get(j)[1];
+                max = Math.max(countStar(r, c), max);
+            }
+        }
+        int answer = K-max;
+        System.out.println(answer);
+    }
+    static int countStar(int r, int c){
+        int cnt = 0;
+        for(int k = 0; k<K;k++){
+            int starR = stars.get(k)[0];
+            int starC = stars.get(k)[1];
+            if(r<=starR&&starR<=r+L && c<=starC&&starC<=c+L) cnt++;
+        }
+        return cnt;
+    }
+
+    static void input(){
         N = sc.nextInt();
         M = sc.nextInt();
         L = sc.nextInt();
@@ -19,22 +42,5 @@ public class Main {
             int y = sc.nextInt();
             stars.add(new int[]{x,y});
         }
-
-        int max = -1;
-        for(int i = 0; i<K;i++){
-            for(int j = 0; j<K;j++){
-                int r = stars.get(i)[0];
-                int c = stars.get(j)[1];
-                int cnt = 0;
-                for(int k = 0; k<K;k++){
-                    int starR = stars.get(k)[0];
-                    int starC = stars.get(k)[1];
-                    if(r<=starR&&starR<=r+L && c<=starC&&starC<=c+L) cnt++;
-                }
-                max = Math.max(cnt, max);
-            }
-        }
-        int answer = K-max;
-        System.out.println(answer);
     }
 }
