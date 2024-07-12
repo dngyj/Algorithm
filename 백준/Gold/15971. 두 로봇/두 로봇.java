edfbef98dea7fe8568601fe02e4672c7e75bff92
정동교 <123816489+dngyj@users.noreply.github.com>
2024-07-12 09:04:52 +0900

    [Gold IV] Title: 두 로봇, Time: 1012 ms, Memory: 247356 KB, Score: 36 point -BaekjoonHub

--- 백준/Gold/15971. 두 로봇/두 로봇.java
+++ 백준/Gold/15971. 두 로봇/두 로봇.java
@@ -1,65 +1,64 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
-	// n이 최대 100이며, 모든 노드는 1개의 부모만 가지고 있으므로 인접행렬을 선언할 수 있음
-	static List<ArrayList<int[]>> adjList = new ArrayList<>();
-	static boolean[] visited;
-	static int N,r1,r2;
-	static int start,find,min;
-	public static void main(String[] args) throws Exception{
-		N = read(); r1 = read(); r2 = read();
-		min = Integer.MAX_VALUE;
-		
-		if(r1==r2) System.out.println(0);
-		else {
-			for(int i = 0; i <= N; i++) {
-				adjList.add(new ArrayList<>());
-			}
-			visited = new boolean[N+1];
-			
-			for(int i = 0 ; i < N-1; i++) {
-				int v = read();
-				int e = read();
-				int c = read();
-				adjList.get(v).add(new int[] {e,c});
-				adjList.get(e).add(new int[] {v,c});
-			}
-			
-			visited[r1] = true;
-			dfs(r1, 0, 0);
-			
-			System.out.print(min);
-		}
-	}
-	
-	static void dfs(int cur, int cost, int max) {
-		if(cur == r2) {
-			min = cost - max;
-			return;
-		}
-		
-		for(int i = 0; i < adjList.get(cur).size(); i++) {
-			if(visited[adjList.get(cur).get(i)[0]]) continue;
-			visited[adjList.get(cur).get(i)[0]] = true;
-			dfs(adjList.get(cur).get(i)[0], cost+adjList.get(cur).get(i)[1], Math.max(max, adjList.get(cur).get(i)[1]));
-		}
-	}
-	
-	
-	// 비트연산을 이용하여 입력을 빠르게 받는 메서드
-	static int read() throws Exception{
-		int n = 0;
-		boolean isNumber = false;
-		while(true) {
-			int c = System.in.read();
-			if(c<=32) {
-				if(isNumber) return n;
-				else continue;
-			}
-			else {
-				isNumber = true;
-				n = (n<<3) + (n<<1) + (c&15);
-			}
-		}
-	}
+    static int N, R1,R2;
+    static ArrayList<Node>[] list;
+    static class Node{
+        int ed;
+        int w;
+        Node(int ed, int w){
+            this.ed = ed;
+            this.w=w;
+        }
+    }
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        R1= sc.nextInt();
+        R2 = sc.nextInt();
+        list = new ArrayList[N+1];
+        for(int i=0; i<N+1;i++){
+            list[i]=new ArrayList<>();
+        }
+        for(int i=0;i<N-1;i++){
+            int st = sc.nextInt();
+            int ed = sc.nextInt();
+            int w = sc.nextInt();
+            list[st].add(new Node(ed,w));
+            list[ed].add(new Node(st,w));
+        }
+        bfs();
+    }
+
+    static void bfs(){
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{R1,0,0});
+        visited = new boolean[N+1];
+        visited[R1] = true;
+        int answer = 0;
+
+        while(!queue.isEmpty()){
+            int[] info = queue.poll();
+            int cur = info[0];
+            int w = info[1];
+            int max = info[2];
+
+            if(cur==R2) answer = w - max;
+
+            for(Node node:list[cur]){
+                int next = node.ed;
+                int w1 = node.w;
+                max = Math.max(w1,max);
+                if(visited[next]) continue;
+                visited[next] = true;
+                queue.add(new int[]{next, w+w1,max});
+            }
+        }
+        System.out.println(answer);
+    }
 }
