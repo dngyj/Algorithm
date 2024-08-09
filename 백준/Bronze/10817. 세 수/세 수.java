89ff443147b17219c42463f22f9cf34df6cb0b9f
정동교 <123816489+dngyj@users.noreply.github.com>
2024-08-09 21:23:47 +0900

    [Bronze III] Title: 세 수, Time: 64 ms, Memory: 11552 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -1,38 +1,19 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-	static BufferedReader br;
-
-	static String endl = "\n";
-	static String blank = " ";
-
-	static int A, B, C;
-
-	static void input() throws IOException {
-		br = new BufferedReader(new InputStreamReader(System.in));
-		stk();
-		A = Integer.parseInt(st.nextToken());
-		B = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-	}
-
-	static void pro() throws IOException {
-		int max = Math.max(Math.max(A, B), C);
-		int min = Math.min(Math.min(A, B), C);
-		System.out.println(A + B + C - max - min);
-
-	}
-
-	public static void main(String[] args) throws IOException {
-		input();
-		pro();
-	}
-
-	static void stk() throws IOException {
-		st = new StringTokenizer(br.readLine());
-	}
-
+    public static void main(String[] args) throws IOException {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+            
+            String text[] = rd.readLine().split(" ");
+            int num[] = new int[3];
+            
+            for(int i=0;i<3;i++) {
+                num[i] = Integer.parseInt(text[i]);
+            }
+            Arrays.sort(num);
+            System.out.println(num[1]);
+    }
 }
