f6f9fb12be03caec1008638704478d47f3052433
정동교 <123816489+dngyj@users.noreply.github.com>
2025-01-22 15:44:12 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 51.37 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,57 +1,52 @@
 import java.util.*;
 
 class Solution {
-    
     static HashMap<String, Integer> map;
     static int[] courseMax;
-    static boolean[] selected;
     static int orderLen;
-    static char[] order;
     static ArrayList<String> ans = new ArrayList<>();
-    
+
     public String[] solution(String[] orders, int[] course) {
         String[] answer;
-        
+
         courseMax = new int[course.length];
         map = new HashMap<>();
-        
+
         for(int i = 0; i<orders.length;i++){
-            order = orders[i].toCharArray();
+            char[] order = orders[i].toCharArray();
             Arrays.sort(order);
             orderLen = order.length;
-            
+
             for(int j = 0; j<course.length;j++){
-                selected = new boolean[orderLen];
-                comb(0,0,course[j], "", j);
+                comb(order,0,0,course[j], "", j);
             }
         }
-        
+
         for(String s: map.keySet()){
-            for(int j = 0; j<course.length;j++){
-               if(courseMax[j]>1&&course[j]==s.length()&&courseMax[j]==map.get(s)){
+            for(int k = 0; k<course.length;k++){
+               if(courseMax[k]>=2 && course[k]==s.length() && courseMax[k]==map.get(s)){
                    ans.add(s);
                }
             }
         }
         Collections.sort(ans);
-        
+
         answer = new String[ans.size()];
         for(int i = 0; i<ans.size();i++){
             answer[i] = ans.get(i);
         }
-        
         return answer;
     }
     
-    static void comb(int idx, int cnt, int N, String s, int j){
+    static void comb(char[] order, int idx, int cnt, int N, String s, int j){
         if(cnt == N){
             map.put(s, map.containsKey(s)?map.get(s)+1:1);
             courseMax[j] = Math.max(courseMax[j], map.get(s));
         }
         else{
             for(int i = idx; i<orderLen;i++){
-                comb(i+1, cnt+1, N, s+order[i], j);
+                comb(order, i+1, cnt+1, N, s+order[i], j);
             }
         }
-    } 
+    }
 }
