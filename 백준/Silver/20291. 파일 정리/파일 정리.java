308da733a5412c97593586ac2aa6a8d2ba693898
정동교 <123816489+dngyj@users.noreply.github.com>
2025-08-01 21:54:28 +0900

    [Silver III] Title: 파일 정리, Time: 1532 ms, Memory: 128672 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -7,21 +7,16 @@ public class Main {
 
         int N = sc.nextInt();
         Map<String, Integer> map = new HashMap<>();
-        ArrayList<String> keyList = new ArrayList<>();
 
         for(int i = 0; i<N; i++){
             String[] strs = sc.next().split("\\.");
             map.put(strs[1], map.getOrDefault(strs[1],0)+1);
         }
 
-        for(String key:map.keySet()){
-            keyList.add(key);
-        }
+        List<String> keyList = new ArrayList<>(map.keySet());
+        Collections.sort(keyList);
 
-        String[] keys = keyList.toArray(new String[0]);
-        Arrays.sort(keys);
-        
-        for(String key : keys){
+        for(String key : keyList){
             System.out.println(key+" "+map.get(key));
         }
     }
